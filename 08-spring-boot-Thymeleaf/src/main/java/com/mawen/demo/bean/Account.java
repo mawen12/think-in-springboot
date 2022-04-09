a648fdf4d81f93499f334e87f28a9a0e63d8cafa
mawen12 <1181963012mw@gmail.com>
2022-04-09 22:40:43 +0800

    Thymeleaf

---
+++ 08-spring-boot-Thymeleaf/src/main/java/com/mawen/demo/bean/Account.java
@@ -0,0 +1,17 @@
+package com.mawen.demo.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+    private String account;
+    private String name;
+    private String password;
+    private String accountType;
+    private String tel;
+
+}
