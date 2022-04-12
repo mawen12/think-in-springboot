33ef74da7376353e9886c11f0a23113e7f26f363
mawen12 <1181963012mw@gmail.com>
2022-04-12 21:21:42 +0800

    Jackson

--- 18-spring-boot-Jackson/src/main/java/com/mawen/demo/config/JacksonConfig.java
+++ 18-spring-boot-Jackson/src/main/java/com/mawen/demo/config/JacksonConfig.java
@@ -1,6 +1,8 @@
 package com.mawen.demo.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.mawen.demo.pojo.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,22 @@ public class JacksonConfig {
     public ObjectMapper getObjectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(User.class, userSerializer());
+        module.addDeserializer(User.class, userDeserializer());
+        mapper.registerModule(module);
+
         return mapper;
+    }
+
+    @Bean
+    public UserSerializer userSerializer() {
+        return new UserSerializer();
+    }
+
+    @Bean
+    public UserDeserializer userDeserializer() {
+        return new UserDeserializer();
     }
 }
